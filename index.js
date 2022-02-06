2b8bd9f095886eaa602b2d2e67c5d04c1f2ad8b1
hjohnsick <hjohnsick@yahoo.com>
2022-02-06 10:00:54 -0500

    added more prompts

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
 const inquirer = require("inquirer");
-const { choices } = require("yargs");
+const Intern = require("./lib/Intern.js");
+const Engineer = require("./lib/Engineer");
+
+const engineerPrompt = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the engineer's name:",
+      },
+      {
+        type: "input",
+        name: "ID",
+        message: "Enter the engineer's ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the engineer's email address:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter the engineer's github username:",
+      },
+    ]);
+  };
+  
+  const internPrompt = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the intern's name:",
+      },
+      {
+        type: "input",
+        name: "ID",
+        message: "Enter the intern's ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the intern's email address:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter the intern's school:",
+      },
+    ]);
+  };
 
 const employeePrompt = () => {
   inquirer
     .prompt([
       {
         type: "list",
-        name: "employee",
-        message: "Choose who you want to add to your team:",
-        choices: ["Intern", "Engineer"],
+        name: "choice",
+        message: "What do you want to do?",
+        choices: ["Add an intern", "Add an engineer", "Finish building out team"],
       },
     ])
-    .then(({ employee }) => {
-      if (employee === "Intern") {
+    .then(({ choice }) => {
+      if (choice === "Add an intern") {
         internPrompt();
-      } else {
+      } else if (choice === "Add an engineer") {
         engineerPrompt();
+      } else {
+          return;
       }
     });
 };
 
-const engineerPrompt = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the engineer's name:",
-    },
-    {
-      type: "input",
-      name: "ID",
-      message: "Enter the engineer's ID:",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the engineer's email address:",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter the engineer's github username:",
-    },
-  ]);
-};
-
-const internPrompt = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the intern's name:",
-    },
-    {
-      type: "input",
-      name: "ID",
-      message: "Enter the intern's ID:",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the intern's email address:",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "Enter the intern's school:",
-    },
-  ]);
-};
-
 const startApplication = () => {
   // prompted to enter managers info at start of application
-  // promptManager()
   inquirer.prompt([
     {
       type: "input",
@@ -93,19 +95,12 @@ const startApplication = () => {
       type: "input",
       name: "officeNumber",
       message: "Enter the manager's office number:",
-    },
-    //   {
-    //     type: "list",
-    //     name: "employee",
-    //     message: "Choose who you want to add to your team:",
-    //     choices: ["Intern", "Engineer"],
-    //   },
+    }
   ])
   .then(() => {
     employeePrompt();
   })
 
- 
 };
 
 startApplication();
